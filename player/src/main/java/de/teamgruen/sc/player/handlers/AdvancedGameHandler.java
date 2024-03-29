77ba60205960874e8e5cde8edb96f5cf4c731bb9
Justus Geramb <admin@justix.dev>
2024-03-29 23:33:31 +0100

    fix(player/advanced): removed multi threading since the contest server doesn't support it

--- player/src/main/java/de/teamgruen/sc/player/handlers/AdvancedGameHandler.java
+++ player/src/main/java/de/teamgruen/sc/player/handlers/AdvancedGameHandler.java
@@ -18,13 +18,8 @@ import de.teamgruen.sc.sdk.protocol.data.board.fields.Passenger;
 import lombok.NonNull;
 
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class AdvancedGameHandler extends BaseGameHandler {
-
-    private static final int CPU_THREADS = Math.max(1, Math.min(4, Runtime.getRuntime().availableProcessors() / 2));
 
     public AdvancedGameHandler(Logger logger) {
         super(logger);
@@ -86,11 +81,10 @@ public class AdvancedGameHandler extends BaseGameHandler {
         final Ship playerShip = gameState.getPlayerShip(), enemyShip = gameState.getEnemyShip();
         final Vector3 shipPosition = playerShip.getPosition();
         final List<LinkedList<Vector3>> paths = new ArrayList<>();
-        final List<Runnable> tasks = new ArrayList<>();
 
         // check if the enemy ship is at least 2 segments ahead
         if(MoveUtil.isEnemyAhead(board, shipPosition, playerShip.getDirection(), enemyShip.getPosition()))
-            tasks.add(() -> paths.add(PathFinder.findPath(playerShip, shipPosition, enemyShip.getPosition())));
+            paths.add(PathFinder.findPath(playerShip, shipPosition, enemyShip.getPosition()));
         // collect passengers and move towards goal after reaching the 4th segment
         else if(board.getSegmentIndex(playerShip.getPosition()) >= 3) {
             final boolean hasEnoughPassengers = playerShip.hasEnoughPassengers();
@@ -113,34 +107,12 @@ public class AdvancedGameHandler extends BaseGameHandler {
                 if(segmentDistance > 2)
                     return;
 
-                tasks.add(() -> paths.add(PathFinder.findPath(playerShip, shipPosition, collectPosition)));
+                paths.add(PathFinder.findPath(playerShip, shipPosition, collectPosition));
             });
         }
 
-        if(!tasks.isEmpty()) {
-            final ExecutorService executorService = Executors.newFixedThreadPool(CPU_THREADS);
-            final CountDownLatch countDownLatch = new CountDownLatch(tasks.size());
-
-            tasks.forEach(task -> executorService.submit(() -> {
-                try {
-                    task.run();
-                } catch (Throwable throwable) {
-                    this.onError(throwable.getMessage());
-                } finally {
-                    countDownLatch.countDown();
-                }
-            }));
-
-            try {
-                countDownLatch.await();
-            } catch (InterruptedException ignore) {
-            } finally {
-                executorService.shutdown();
-            }
-
-            // remove invalid paths
-            paths.removeIf(path -> path == null || path.size() < 2);
-        }
+        // remove invalid paths
+        paths.removeIf(path -> path == null || path.size() < 2);
 
         return paths;
     }
